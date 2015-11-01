e23865f37fcac2ba608bb24d713a9c358ba31919
The Fart Maker <fartnaker@ifartonyou.com>
2015-11-01 19:01:58 +0100

    added material design and welcome page prototipe

---
+++ resources/views/app.blade.php
@@ -0,0 +1,44 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>VPN hourly</title>
+        <link rel="stylesheet" type="text/css" href="css/materialize.css">
+    </head>
+
+    <body>
+
+        <nav>
+            <div class="nav-wrapper  green darken-1">
+                <a href="#!" class="brand-logo"><img src="images/lock.png"></a>
+                <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
+                <ul class="right hide-on-med-and-down">
+                    <li><a href="#!"">Prcing</a></li>
+                    <li><a href="#!"">Features</a></li>
+                    <li><a href="#!"">Help</a></li>
+                    <li><a href="#!"">Log In</a></li>
+                    <li><a href="#!"">Sign Up</a></li>
+                </ul>
+                <ul class="side-nav" id="mobile-demo">
+                    <li><a href="#!"">Prcing</a></li>
+                    <li><a href="#!"">Features</a></li>
+                    <li><a href="#!"">Help</a></li>
+                    <li><a href="#!"">Log In</a></li>
+                    <li><a href="#!"">Sign Up</a></li>
+                </ul>
+            </div>
+        </nav>
+
+        <div class="container">
+            @yield('content')
+        </div>
+
+        <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+        <script type="text/javascript" src="js/bin/materialize.js"></script>
+        <script>
+            $( document ).ready(function(){
+                $(".button-collapse").sideNav();
+            })
+
+        </script>
+    </body>
+</html>
